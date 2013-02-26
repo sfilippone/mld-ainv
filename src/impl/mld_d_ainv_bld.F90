737931776b484082e31648a91b7700379dae295a
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2013-02-26 10:50:22 +0000

    mld-ainv: M    Make.inc A  + src/impl/mld_d_ainv_bld.F90 D    src/impl/mld_d_ainv_bld.f90
    Fixed bld procedure on condition for TUMA sainv.

--- src/impl/mld_d_ainv_bld.F90
+++ src/impl/mld_d_ainv_bld.F90
@@ -150,9 +150,14 @@ subroutine mld_d_ainv_bld(a,alg,fillin,thresh,wmat,d,zmat,desc,info,blck,iscale)
          &   wmat,nzrmax,sp_thresh,info)
     call wmat%transp()
   case(mld_ainv_llk_,mld_ainv_s_llk_,mld_ainv_s_ft_llk_,&
-       & mld_ainv_llk_noth_,mld_ainv_tuma_)
+       & mld_ainv_llk_noth_)
     call mld_sparse_biconjg(alg,n_row,acsr,pq,&
          &   zmat,wmat,nzrmax,sp_thresh,info)
+#ifdef HAVE_TUMA_SAINV
+  case(mld_ainv_tuma_)
+    call mld_sparse_biconjg(alg,n_row,acsr,pq,&
+         &   zmat,wmat,nzrmax,sp_thresh,info)
+#endif
   case default
     info = psb_err_internal_error_
     call psb_errpush(info,name,a_err='ainv_bld: bad alg')
