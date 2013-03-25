84b35c73ec00eed894e360054d6c11343c6fb5b9
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2013-03-25 16:08:20 +0000

    mld-ainv:  src/impl/mld_d_base_ainv_solver_apply.f90  src/impl/mld_d_base_ainv_solver_apply_vect.f90  src/impl/mld_d_base_ainv_solver_dmp.f90  src/impl/mld_d_base_ainv_solver_free.f90  src/mld_d_base_ainv_mod.F90
    Missing data on merge?

---
+++ src/impl/mld_d_base_ainv_solver_dmp.f90
@@ -0,0 +1,85 @@
+!!$
+!!$ 
+!!$                     MLD-AINV: Approximate Inverse plugin for
+!!$                           MLD2P4  version 2.0
+!!$  
+!!$  (C) Copyright 2012
+!!$
+!!$                      Salvatore Filippone  University of Rome Tor Vergata
+!!$ 
+!!$  Redistribution and use in source and binary forms, with or without
+!!$  modification, are permitted provided that the following conditions
+!!$  are met:
+!!$    1. Redistributions of source code must retain the above copyright
+!!$       notice, this list of conditions and the following disclaimer.
+!!$    2. Redistributions in binary form must reproduce the above copyright
+!!$       notice, this list of conditions, and the following disclaimer in the
+!!$       documentation and/or other materials provided with the distribution.
+!!$    3. The name of the MLD2P4 group or the names of its contributors may
+!!$       not be used to endorse or promote products derived from this
+!!$       software without specific written permission.
+!!$ 
+!!$  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+!!$  ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+!!$  TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+!!$  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE MLD2P4 GROUP OR ITS CONTRIBUTORS
+!!$  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+!!$  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+!!$  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+!!$  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+!!$  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+!!$  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+!!$  POSSIBILITY OF SUCH DAMAGE.
+!!$ 
+!!$
+subroutine mld_d_base_ainv_solver_dmp(sv,ictxt,level,info,prefix,head,solver)
+  
+  use psb_base_mod
+  use mld_d_base_ainv_mod, mld_protect_name => mld_d_base_ainv_solver_dmp
+  implicit none 
+  class(mld_d_base_ainv_solver_type), intent(in) :: sv
+  integer, intent(in)              :: ictxt,level
+  integer, intent(out)             :: info
+  character(len=*), intent(in), optional :: prefix, head
+  logical, optional, intent(in)    :: solver
+  integer :: i, j, il1, iln, lname, lev
+  integer :: icontxt,iam, np
+  character(len=80)  :: prefix_
+  character(len=120) :: fname ! len should be at least 20 more than
+  logical :: solver_
+  !  len of prefix_ 
+
+  info = 0
+
+  if (present(prefix)) then 
+    prefix_ = trim(prefix(1:min(len(prefix),len(prefix_))))
+  else
+    prefix_ = "dump_ainv_d"
+  end if
+
+  call psb_info(ictxt,iam,np)
+
+  if (present(solver)) then 
+    solver_ = solver
+  else
+    solver_ = .false. 
+  end if
+  lname = len_trim(prefix_)
+  fname = trim(prefix_)
+  write(fname(lname+1:lname+5),'(a,i3.3)') '_p',iam
+  lname = lname + 5
+
+  if (solver_) then 
+    write(fname(lname+1:),'(a,i3.3,a)')'_l',level,'_wmat.mtx'
+    if (sv%w%is_asb()) &
+         & call sv%w%print(fname,head=head)
+    write(fname(lname+1:),'(a,i3.3,a)')'_l',level,'_diag.mtx'
+    if (allocated(sv%d)) &
+         & call psb_geprt(fname,sv%d,head=head)
+    write(fname(lname+1:),'(a,i3.3,a)')'_l',level,'_zmat.mtx'
+    if (sv%z%is_asb()) &
+         & call sv%z%print(fname,head=head)
+
+  end if
+
+end subroutine mld_d_base_ainv_solver_dmp
